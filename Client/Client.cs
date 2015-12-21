048d004f9647614775ac55f0e198cba8b1dfcaff
Ola <olasojka@gmail.com>
2015-12-21 19:47:25 +0100

    ftrasWEGRTH
    EETYJYU

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,122 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+//using System.Timers;
 
 namespace Client
 {
     class Client
     {
-        private static String nick;
-
-        static void UdpSend()
-        {
-            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint send = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7);
-            s.SendTo(Encoding.ASCII.GetBytes("DISCOVER"), send);
-            Console.WriteLine("Discover message sent to the broadcast address");
-        }
-
-        static bool UdpReceive()
-        {
-             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-             EndPoint recv = (EndPoint)(new IPEndPoint(IPAddress.Any, 0));
-             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7);
-             s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 10000);
-             s.Bind(endPoint);
-            // s.ReceiveTimeout = 10000;
-
-             byte[] buff = new byte[20];
-             int i = 0;
-
-             Console.WriteLine("Waiting for offer...");
-             try
-             {
-                 s.ReceiveFrom(buff, ref recv);
-                 String msg = Encoding.ASCII.GetString(buff).TrimEnd('\0');
-                 if (msg.Equals("OFFER"))
-                 {
-                     Console.WriteLine("Received offer from {0}", recv.ToString());
-                 }
-             }
-             catch (SocketException e)
-             {
-                 if (e.SocketErrorCode == SocketError.TimedOut)
-                 {
-                     Console.WriteLine("No server was found. Trying once again.");
-                     s.Close();
-                     return false;
-                 }
-             }
-
-            /*bool done = false;
-
-            UdpClient listener = new UdpClient(7);
-            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 7);
-            listener.
-            try
-            {
-                while (!done)
-                {
-                    Console.WriteLine("Waiting for broadcast");
-                    byte[] bytes = listener.Receive(ref groupEP);
-
-                    Console.WriteLine("Received broadcast from {0} :\n {1}\n",
-                        groupEP.ToString(),
-                        Encoding.ASCII.GetString(bytes, 0, bytes.Length));
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                listener.Close();
-            }*/
-
-            return true;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your nick:");
-            nick = Console.ReadLine();
-
-            bool serverFound = false;
-
-            while (!serverFound)
-            {
-                UdpSend();
-                serverFound = UdpReceive();
-            }
-
-            /*  while (!done)
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            udpSocket.ReceiveTimeout = 1000;
+            IPAddress ip = IPAddress.Broadcast;
+            IPEndPoint udpSend = new IPEndPoint(ip, 7);
+            udpSocket.SendTo(Encoding.ASCII.GetBytes("DISCOVER"), udpSend);
+            Console.WriteLine("Broadcast DISCOVER message sent.");
+            byte[] buff = new byte[20];
+            EndPoint recv = (EndPoint)(new IPEndPoint(IPAddress.Any, 0));
+            //while (true)
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            int tcpport = 0;
+            while (s.Elapsed < TimeSpan.FromSeconds(10))
             {
                 try
                 {
-                    Console.WriteLine("Waiting for offer...");
-                    if (s.Available != 0)
+                    udpSocket.ReceiveFrom(buff, ref recv);
+                    String[] msg = Encoding.ASCII.GetString(buff).TrimEnd('\0').Split('-');
+                    tcpport = Convert.ToInt32(msg[1]);
+                    Console.WriteLine(msg[0]);
+                }
+                catch (SocketException e)
+                {
+                    if (e.SocketErrorCode == SocketError.TimedOut)
                     {
-                        s.ReceiveFrom(buff, ref recv);
-                        String msg = Encoding.ASCII.GetString(buff).TrimEnd('\0');
-                        if (msg.Equals("OFFER"))
-                        {
-                            Console.WriteLine("Received offer from {0}\n", recv.ToString());
-                            done = true;
-                        }
+                       //break;
                     }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine("No server was found");
-                    done = true;
-                }
-            }*/
+            }
+            Console.WriteLine("Olka chuj");
+            s.Stop();
+            IPAddress tcpip = IPAddress.Parse(recv.ToString().Split(':')[0].TrimEnd(' '));
+            IPEndPoint tcEndPoint = new IPEndPoint(tcpip, tcpport);
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(tcEndPoint);
+            sender.Send(Encoding.ASCII.GetBytes("dupa"));
+            Console.WriteLine("Wyslalem dupe");
 
 
 
--- NetworkProgramming2/Server.cs
+++ NetworkProgramming2/Server.cs
@@ -11,31 +11,76 @@ namespace NetworkProgramming2
         private static void UDPListener()
         {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] ipList = Dns.GetHostAddresses(Dns.GetHostName());
+            IPAddress ipchuj = null;
+            foreach (var ip in ipList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine(ip.ToString());
+                    ipchuj = ip;
+                }
+            }
             EndPoint recv = (EndPoint) (new IPEndPoint(IPAddress.Any, 0));
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7);
+            s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             s.Bind(endPoint);
 
             while (true)
             {
                 byte[] buff = new byte[20];
-                int i = 0;
-
+                int port = 5005;
                 s.ReceiveFrom(buff, ref recv);
                 String msg = Encoding.ASCII.GetString(buff).TrimEnd('\0');
                 //Console.WriteLine(msg + "..............");
                 if (msg.Equals("DISCOVER"))
                 {
                     Console.WriteLine("Received broadcast from {0} : {1}", recv.ToString(), msg);
-                    s.SendTo(Encoding.ASCII.GetBytes("OFFER"), recv);
+                    s.SendTo(Encoding.ASCII.GetBytes("OFFER-" + port), recv);
+                    Thread tcp = new Thread(
+                        o =>
+                        {
+                            tcpchuj(ipchuj, port);
+                        });
+                    tcp.Start();
                 }
             }
         }
 
+        public static void tcpchuj(IPAddress ip, int port)
+        {
+            String data = null;
+            Socket tcpsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpsocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            IPEndPoint tcpendEndPoint = new IPEndPoint(ip, port);
+            tcpsocket.Bind(tcpendEndPoint);
+            tcpsocket.Listen(1);
+            Socket handler = tcpsocket.Accept();
+            byte[] bytes = new byte[1024];
+            int bytesRec = handler.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            Console.WriteLine("Text received : {0}", data);
+
+        }
+
         public static int Main(String[] args)
         {
-            Thread udp = new Thread(new ThreadStart(UDPListener));
-            udp.Start();
+            //Thread udp = new Thread(new ThreadStart(UDPListener));
+            UDPListener();
+            //udp.Start();
+            //Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
 
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7);
+            //s.Bind(endPoint);
+            //byte[] buff = new byte[20];
+
+            //while (true)
+            //{
+            //    s.ReceiveFrom()
+            //}
 
             Console.ReadKey();
             return 0;
