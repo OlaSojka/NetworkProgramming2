918bb37b54ea145a4d5609c724b3c2edb709ace3
Ola <olasojka@gmail.com>
2015-12-22 00:58:09 +0100

    erdfh

--- Client/Client.cs
+++ Client/Client.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-//using System.Timers;
+using System.IO;
 
 namespace Client
 {
@@ -14,19 +14,19 @@ namespace Client
     {
         private static Socket udpSocket;
 
-        static void Main(string[] args)
+        static void UdpSend()
         {
-            udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
-            udpSocket.ReceiveTimeout = 1000;
             IPAddress ip = IPAddress.Broadcast;
             IPEndPoint udpSend = new IPEndPoint(ip, 7);
             udpSocket.SendTo(Encoding.ASCII.GetBytes("DISCOVER"), udpSend);
             Console.WriteLine("Broadcast DISCOVER message sent.");
+        }
+
+        static List<Tuple<EndPoint,int>> UdpReceive() //tuple(recv, port)
+        {
             byte[] buff = new byte[20];
             EndPoint recv = (EndPoint)(new IPEndPoint(IPAddress.Any, 0));
-            //while (true)
+            List<Tuple<EndPoint, int>> servers = new List<Tuple<EndPoint, int>>();
             Stopwatch s = new Stopwatch();
             s.Start();
             int tcpport = 0;
@@ -37,7 +37,8 @@ namespace Client
                     udpSocket.ReceiveFrom(buff, ref recv);
                     String[] msg = Encoding.ASCII.GetString(buff).TrimEnd('\0').Split('-');
                     tcpport = Convert.ToInt32(msg[1]);
-                    Console.WriteLine(msg[0]);
+                    Console.WriteLine("Received {0} from: {1}", msg[0], recv);
+                    servers.Add(Tuple.Create<EndPoint, int>(recv, tcpport));
                 }
                 catch (SocketException e)
                 {
@@ -46,18 +47,78 @@ namespace Client
                         Console.WriteLine(e.StackTrace);
                 }
             }
-            Console.WriteLine("Olka chuj");
             s.Stop();
+            if (servers.Count == 0)
+            {
+                Console.WriteLine("No server found, trying once again");
+                return null;
+            }
+            return servers;
+        }
+
+        private static void writeLastServer(IPEndPoint ep)
+        {
+            string path = "lastserver.txt";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            FileStream fs = null;
+            fs = new FileStream(path, FileMode.CreateNew);
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.Write(ep.ToString());
+            }
+        }
+
+        static bool TcpSender(EndPoint recv, int tcpport)
+        {
             IPAddress tcpip = IPAddress.Parse(recv.ToString().Split(':')[0].TrimEnd(' '));
-            IPEndPoint tcEndPoint = new IPEndPoint(tcpip, tcpport);
+            IPEndPoint tcpEndPoint = new IPEndPoint(tcpip, tcpport);
             Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
-                sender.Connect(tcEndPoint);
+                sender.Connect(tcpEndPoint);
+
+                writeLastServer(tcpEndPoint);
+
+                Console.WriteLine("Connection established. Enter your nick:");
+                String nick = Console.ReadLine();
+                Console.WriteLine("Give the frequency in which you want to send data (in miliseconds, between 0 and 10000):");
+                bool flag = false;
+                int frequency = 0;
+                while (!flag)
+                {
+                    String input = Console.ReadLine();
+                    try
+                    {
+                        int number = Int32.Parse(input);
+                        if (number < 0)
+                            Console.WriteLine("Frequency cannot be nagative, try again");
+                        else if(number > 10000)
+                            Console.WriteLine("Frequency cannot be bigger than 10000ms, try again");
+                        else
+                        {
+                            frequency = number;
+                            flag = true;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("It's not a number, try again.");
+                    }
+                }
+
+                sender.Send(Encoding.ASCII.GetBytes("NICK:" + nick));
+
                 while (true)
                 {
-                    sender.Send(Encoding.ASCII.GetBytes("dupa"));
-                    Console.WriteLine("Wyslalem dupe");
+                    Random rnd = new Random();
+                    int value = rnd.Next(0, 101);
+                    sender.Send(Encoding.ASCII.GetBytes(value.ToString()));
+                    Console.WriteLine("VALUE {0} sent", value);
+                    Thread.Sleep(frequency);
                 }
             }
             catch (SocketException e)
@@ -65,8 +126,108 @@ namespace Client
                 if (e.SocketErrorCode == SocketError.ConnectionRefused ||
                     e.SocketErrorCode == SocketError.ConnectionReset)
                 {
-                    Console.WriteLine("no nie pyklo");
+                    Console.WriteLine("Server disconnected.");
+                    return false;
                 }
+            }
+            return true;
+        }
+
+        static void SuggestServer()
+        {
+            string path = "lastserver.txt";
+            String server = null;
+
+            if (File.Exists(path))
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (sr.Peek() >= 0)
+                        {
+                            server = sr.ReadLine();
+                        }
+                    }
+                }
+                if (server != null)
+                {
+                    Console.WriteLine("Suggested connection: {0}.\nDo you want to connect? y/n", server);
+                    bool flag = false;
+                    while (!flag)
+                    {
+                        ConsoleKeyInfo answer = Console.ReadKey();
+                        if (answer.KeyChar == 'y')
+                        {
+                            int port = Int32.Parse(server.Split(':')[1]);
+                            EndPoint ep = new IPEndPoint(IPAddress.Parse(server.Split(':')[0]), port);
+                            flag = true;
+                            TcpSender(ep, port);
+                        }
+                        else if (answer.KeyChar == 'n')
+                            flag = true;
+                        else
+                            flag = false;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            udpSocket.ReceiveTimeout = 1000;
+            
+            bool serverConnected = false;
+            while (!serverConnected)
+            {
+                SuggestServer();
+
+                bool serverFound = false;
+                List<Tuple<EndPoint, int>> serversList = new List<Tuple<EndPoint, int>>();
+
+                while (!serverFound)
+                {
+                    UdpSend();
+                    serversList = UdpReceive();
+                    if (serversList != null)
+                        serverFound = true;
+                }
+                Console.WriteLine("List of available servers:");
+                int counter = 1;
+                foreach (var server in serversList)
+                {
+                    Console.WriteLine("{0}. {1}:{2}", counter, server.Item1.ToString().Split(':')[0], server.Item2);
+                    counter++;
+                }
+                Console.WriteLine("Select server you want to connect to:");
+                bool flag = false;
+                int selectedServer = 0;
+                while (!flag)
+                {
+                    String input = Console.ReadLine();
+                    try
+                    {
+                        int server = Int32.Parse(input);
+                        if (server > serversList.Count)
+                            Console.WriteLine("The number is too big, try again.");
+                        else if (server < 0)
+                            Console.WriteLine("The number cannot be nagative, try again");
+                        else
+                        {
+                            selectedServer = server - 1;
+                            flag = true;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("It's not a number, try again.");
+                    }
+                }
+
+                serverConnected = TcpSender(serversList.ElementAt(selectedServer).Item1, serversList.ElementAt(selectedServer).Item2);
             }
             Console.ReadKey();
         }
--- NetworkProgramming2/Server.cs
+++ NetworkProgramming2/Server.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading;
 
@@ -11,83 +14,111 @@ namespace NetworkProgramming2
         private static void UDPListener()
         {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            //IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
-            var host = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress[] ipList = Dns.GetHostAddresses(Dns.GetHostName());
-            IPAddress ipchuj = null;
-            foreach (var ip in ipList)
+            IPAddress ip = null;
+            foreach (var x in ipList)
             {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                if (x.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    Console.WriteLine(ip.ToString());
-                    ipchuj = ip;
+                 //   Console.WriteLine(ip.ToString());
+                    ip = x;
                 }
             }
+            Console.WriteLine("My IP address is: {0}, and I'm listening on port 7", ip);
             EndPoint recv = (EndPoint) (new IPEndPoint(IPAddress.Any, 0));
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7);
             s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             s.Bind(endPoint);
+            int port = 5005;
 
             while (true)
             {
                 byte[] buff = new byte[20];
-                int port = 5005;
                 s.ReceiveFrom(buff, ref recv);
                 String msg = Encoding.ASCII.GetString(buff).TrimEnd('\0');
                 //Console.WriteLine(msg + "..............");
                 if (msg.Equals("DISCOVER"))
                 {
+                    int nowyport = port;
                     Console.WriteLine("Received broadcast from {0} : {1}", recv.ToString(), msg);
                     s.SendTo(Encoding.ASCII.GetBytes("OFFER-" + port), recv);
                     Thread tcp = new Thread(
                         o =>
                         {
-                            tcpchuj(ipchuj, port);
+                            tcpConnect(ip, nowyport);
                         });
+                    port++;
                     tcp.Start();
                 }
             }
         }
 
-        public static void tcpchuj(IPAddress ip, int port)
+        public static bool PortInUse(int port)
         {
-            String data = null;
+            bool inUse = false;
+
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+
+            foreach (IPEndPoint endPoint in ipEndPoints)
+            {
+                if (endPoint.Port == port)
+                {
+                    inUse = true;
+                    break;
+                }
+            }
+            return inUse;
+        }
+
+        public static void tcpConnect(IPAddress ip, int port)
+        {
+           // Console.WriteLine(port);
             Socket tcpsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             tcpsocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             IPEndPoint tcpendEndPoint = new IPEndPoint(ip, port);
             tcpsocket.Bind(tcpendEndPoint);
             tcpsocket.Listen(1);
-            Socket handler = tcpsocket.Accept();
-            byte[] bytes = new byte[1024];
-            while (true)
+            String nick = "";
+            try
             {
+                Socket handler = tcpsocket.Accept();
+                byte[] bytes = new byte[1024];
                 int bytesRec = handler.Receive(bytes);
-                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                Console.WriteLine("Text received : {0}", data);
+                nick = Encoding.ASCII.GetString(bytes, 0, bytesRec).Split(':')[1];
+                while (true)
+                {
+                    int bytesRec2 = handler.Receive(bytes);
+                    Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRec2));
+                }
             }
-            
-
+            catch (SocketException e)
+            {
+                if (e.SocketErrorCode == SocketError.ConnectionRefused ||
+                    e.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    Console.WriteLine("{0} disconnected.", nick);
+                }
+            }
         }
+
+       /* static void CheckTcp()
+        {
+            Socket tcpsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpsocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            while(!tcpsocket.Poll(0, SelectMode.SelectRead))
+                Console.WriteLine(tcpsocket.Poll(0, SelectMode.SelectRead));
+        }*/
 
         public static int Main(String[] args)
         {
-            //Thread udp = new Thread(new ThreadStart(UDPListener));
-            UDPListener();
-            //udp.Start();
-            //Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            //s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-
-            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7);
-            //s.Bind(endPoint);
-            //byte[] buff = new byte[20];
-
-            //while (true)
-            //{
-            //    s.ReceiveFrom()
-            //}
+            Thread udp = new Thread(new ThreadStart(UDPListener));
+            //UDPListener();
+            udp.Start();
 
             Console.ReadKey();
             return 0;
+
         }
     }
     
